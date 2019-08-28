975af5708770444dae1fe9808892f38eab7b761c
Ubaid-ur-Rehman Butt <ubutt@alphabold.com>
2019-08-28 09:35:06 +0500

    minor change in server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {isRealString} = require ('./utils/validation');
 const {Users} = require ('./utils/users');
 
 const publicPath = path.join (__dirname, '../public');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer (app);
 const io = socketIO (server); 
